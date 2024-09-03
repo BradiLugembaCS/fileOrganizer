afad1062f0b56af892c94435c55c7deb48dc5886
BradiLugembaTUD <bradi.lugemba@gmail.com>
2024-09-03 20:37:59 +0100

    test files and iteration
    test directory and files and iterating through the all the files in the directory

--- fileOrganizer.py
+++ fileOrganizer.py
@@ -1,9 +1,13 @@
 import os
 
+#Aim of the project
 print("This program tidies up files in a specified directory by organizing them into folders based on their file type/extensions.")
-dirName = input("Please enter the path of the directory: ")
+path = input("Please enter the path of the directory: ")
 
-if os.path.exists(dirName) == True:
-    print("YUP")
+if os.path.exists(path) == True:
+    #Looping the files of the path
+    for root, dirs, files in os.walk(path):
+        for _file in files:
+            print(_file)
 else:
     print("Nope")
