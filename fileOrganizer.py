aa9b0cab68637d1b9542735ab67fb8577aeb9680
BradiLugembaTUD <bradi.lugemba@gmail.com>
2024-09-03 20:04:00 +0100

    Update fileOrganizer.py
    importing library and stating the aim of the project

---
+++ fileOrganizer.py
@@ -0,0 +1,4 @@
+import os
+
+print("This program tidies up files in a specified directory by organizing them into folders based on their file type/extensions.")
+dirName = input("Please enter the name of the directory: ")
