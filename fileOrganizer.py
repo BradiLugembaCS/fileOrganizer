c50e913a48545f2ae4c0d0b8e3207546f892f022
BradiLugembaTUD <bradi.lugemba@gmail.com>
2024-09-03 20:13:00 +0100

    path exists
    Checking if the path of the directory  exists

--- fileOrganizer.py
+++ fileOrganizer.py
@@ -1,4 +1,9 @@
 import os
 
 print("This program tidies up files in a specified directory by organizing them into folders based on their file type/extensions.")
-dirName = input("Please enter the name of the directory: ")
+dirName = input("Please enter the path of the directory: ")
+
+if os.path.exists(dirName) == True:
+    print("YUP")
+else:
+    print("Nope")
