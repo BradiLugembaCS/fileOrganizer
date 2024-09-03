51e5f5bef2a8b2deb0c9094d1e633a2f8756df7d
BradiLugembaTUD <bradi.lugemba@gmail.com>
2024-09-03 20:43:00 +0100

    extensions
    splitting the file name to get the extension
    
    creating a dictionary to map all the file extensions

--- fileOrganizer.py
+++ fileOrganizer.py
@@ -4,10 +4,14 @@ import os
 print("This program tidies up files in a specified directory by organizing them into folders based on their file type/extensions.")
 path = input("Please enter the path of the directory: ")
 
+mapping = { 'pdf': 'PDFs', 'jpg': 'Images', 'png': 'Images', 'py': 'Python', 'c' : 'C code'}
+
 if os.path.exists(path) == True:
     #Looping the files of the path
     for root, dirs, files in os.walk(path):
         for _file in files:
             print(_file)
+            extension = _file.split(".")
+            print(extension[1])
 else:
     print("Nope")
